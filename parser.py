5b1751619cee1fb116f6371aab85a06e7281f910
Filip Cák <71824428+filipcak@users.noreply.github.com>
2023-10-24 22:06:37 +0200

    parsed final

--- parser.py
+++ parser.py
@@ -1,59 +1,78 @@
 import os
 import json
 import re
+import csv
 
 
 class Parser:
     def __init__(self):
-        self.folder_path = 'test_data/'
+        self.folder_path = 'data/'
+        self.csv_file_path = 'parsed/test_data.csv'
+        self.regex_keys = ["title", "year", "date", "startTime", "avgSpeed", "distance", "raceCategory", "profileScore",
+                           "verticalMeters", "departure", "arrival", "raceRanking", "startlistScore", "howWon",
+                           "avgTemperature", "winnerTime", "stageNumber"]
+        self.regex_keys_stage = ["nameRider", "teamRider", "flagRider", "bibNumberRider"]
+        self.data_list = []
         with open('regex.json', 'r', encoding='utf-8') as json_file:
             self.regex = json.load(json_file)
 
-    def parser(self):
-        folder_path = self.folder_path
+    # parse data from downloaded pages
+    def parse(self):
+        # all files in the folder
+        files = os.listdir(self.folder_path)
 
-        # List all files in the folder
-        files = os.listdir(folder_path)
-
-        # Filter for .txt files
+        # filter .txt files - just to be sure
         txt_files = [file for file in files if file.endswith('.txt')]
 
-        # Process each .txt file
-        for i, txt_file in enumerate(txt_files):
-            if i >= 5:  # Stop processing after the first 10 files
-                break
-            file_path = os.path.join(folder_path, txt_file)
+        # Process each file
+        for txt_file in txt_files:
+            file_path = os.path.join(self.folder_path, txt_file)
+            # open file to read
             with open(file_path, 'r') as file:
                 file_contents = file.read()
-                title_match = re.findall(self.regex['title'], file_contents)
-                year = re.findall(self.regex['year'], file_contents)
-                date = re.findall(self.regex['date'], file_contents)
-                start_time = re.findall(self.regex['startTime'], file_contents)
-                avg_speed = re.findall(self.regex['avgSpeed'], file_contents)
-                distance = re.findall(self.regex['distance'], file_contents)
-                race_category = re.findall(self.regex['raceCategory'], file_contents)
-                profile_score = re.findall(self.regex['profileScore'], file_contents)
-                vertical_meters = re.findall(self.regex['verticalMeters'], file_contents)
-                departure = re.findall(self.regex['departure'], file_contents)
-                arrival = re.findall(self.regex['arrival'], file_contents)
-                race_ranking = re.findall(self.regex['raceRanking'], file_contents)
-                startlist_score = re.findall(self.regex['startlistScore'], file_contents)
-                how_won = re.findall(self.regex['howWon'], file_contents)
-                avg_temperature = re.findall(self.regex['avgTemperature'], file_contents)
-                # take just first element of the list
-                winner_time = re.findall(self.regex['winnerTime'], file_contents)
-                name_place = re.findall(self.regex['namePlace'].replace("placeholder", "9"), file_contents)
-                team_place = re.findall(self.regex['teamPlace'].replace("placeholder", "9"), file_contents)
-                is_stage = re.findall(self.regex['isStage'], file_contents)
-                if is_stage:
-                    name_place = re.findall(self.regex['stage']['namePlace'].replace("placeholder", "9"), file_contents)
-                    team_place = re.findall(self.regex['stage']['teamPlace'].replace("placeholder", "9"), file_contents)
-                #print(title_match, year, date)
-                #print(name_place, is_stage)
+                # dictionary to store in csv
+                row = {}
+                # add default information
+                for key in self.regex_keys:
+                    try:
+                        row[key] = re.findall(self.regex[key], file_contents)[0].strip()
+                    except IndexError:
+                        row[key] = ''
+                # add values based on race type for 10 riders
+                for i in range(1, 11):
+                    for key in self.regex_keys_stage:
+                        # if it is stage - change the regex
+                        if row['stageNumber'] != '':
+                            try:
+                                row[f'{key}_{i}'] = re.findall(self.regex['stage'][key].replace("placeholder", str(i)),
+                                                               file_contents)[0]
+                            except IndexError:
+                                row[f'{key}_{i}'] = ''
+                        else:
+                            try:
+                                row[f'{key}_{i}'] = re.findall(self.regex[key].replace("placeholder", str(i)),
+                                                               file_contents)[0]
+                            except IndexError:
+                                row[f'{key}_{i}'] = ''
+                # for each file append to data_list to store at the end to csv file
+                self.data_list.append(row)
+
+    # save to csv
+    def save_to_csv(self):
+        # Define the column names
+        fieldnames = self.data_list[0].keys()
+
+        # Write the data to the CSV file
+        with open(self.csv_file_path, 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            # write data headers
+            writer.writeheader()
+            # write the data rows
+            writer.writerows(self.data_list)
 
     def run(self):
-        # Initial URL to visit
-        self.parser()
+        self.parse()
+        self.save_to_csv()
 
 
 if __name__ == '__main__':

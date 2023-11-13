1a4b3195f6d79d8221e97b6ba02241105dc8fcf0
Filip Cák <71824428+filipcak@users.noreply.github.com>
2023-11-13 22:27:30 +0100

    indexer

---
+++ crawler.py
@@ -0,0 +1,106 @@
+import requests
+import re
+import json
+from datetime import datetime
+from collections import deque
+import time
+
+
+# Function to download and store a webpage
+def download_and_store(url, visited_pages, initial_url, max_urls, found_pages):
+    queue = deque([url])
+    counter = 0
+    params = {
+        "contact": "true",
+        "email": "xcak@stuba.sk",
+        "project": "VINF",
+        "university": "Slovak University of Technology",
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    }
+
+    for found_page in found_pages:
+        if found_page not in visited_pages:
+            queue.append(initial_url + found_page)
+
+    while queue and len(visited_pages) < max_urls:
+        url = queue.popleft()
+
+        # Check if URL already visited
+        if url not in visited_pages or url == initial_url:
+            # Increment the counter
+            counter += 1
+            # Check if the counter is a multiple of 990
+            if counter % 200 == 0:
+                print("Sleeping for 30 seconds...")
+                time.sleep(30)
+            # Make the GET request
+            try:
+                print(url)
+                response = requests.get(url, headers=headers, params=params, timeout=10)
+                print(response.status_code)
+                # Check if the request was successful
+                if response.status_code == 200:
+                    # Access the content of the webpage
+                    webpage_content = response.text
+
+                    # Store the HTML content in a txt file
+                    if url != initial_url:
+                        filename = f'page_{datetime.utcnow().strftime("%Y%m%d_%H%M%S%f")}.txt'
+                        with open("data/" + filename, 'w+', encoding='utf-8') as file:
+                            file.write(webpage_content)
+                        print(f"Stored '{filename}'")
+
+                    # Mark the URL as visited to avoid revisiting it
+                    visited_pages.add(url)
+
+                    # Use regular expressions to find links with "race" in href attribute
+                    url_pattern = re.compile(r'href=["\'](race/[^\s"\'<>]+/[\d]+/result|race/[^\s"\'<>]+/[\d]+/gc'
+                                             r'|race/[^\s"\'<>]+/[\d]+/stage-[\d])')
+                    matched_urls = url_pattern.findall(webpage_content)
+
+                    # Add matched URLs to the queue for further processing
+                    for matched_url in matched_urls:
+                        if matched_url not in found_pages:
+                            found_pages.add(matched_url)
+                            matched_url = initial_url + matched_url
+                            queue.append(matched_url)
+                else:
+                    print(f"Failed to retrieve webpage. Status code: {response.status_code}")
+            except Exception as e:
+                print(f"Request error: {e}")
+
+
+def run():
+    # Initial URL to visit
+    initial_url = 'https://procyclingstats.com/'
+    max_urls = 1000
+
+    try:
+        with open('visited_urls.json', 'r', encoding='utf-8') as json_file:
+            visited_pages = set(json.load(json_file))
+            max_urls += len(visited_pages)
+    except FileNotFoundError:
+        visited_pages = set()
+
+    try:
+        with open('found_urls.json', 'r', encoding='utf-8') as json_file:
+            found_pages = set(json.load(json_file))
+    except FileNotFoundError:
+        found_pages = set()
+
+    # Start downloading and storing pages from the initial URL, limiting to 5 URLs
+    download_and_store('https://www.procyclingstats.com/', visited_pages, initial_url, max_urls, found_pages)
+
+    # Store the visited URLs in a JSON file
+    with open('visited_urls.json', 'w', encoding='utf-8') as json_file:
+        json.dump(list(visited_pages), json_file, indent=4)
+
+    # Store the found URLs in a JSON file
+    with open('found_urls.json', 'w', encoding='utf-8') as json_file:
+        json.dump(list(found_pages), json_file, indent=4)
+
+
+if __name__ == '__main__':
+    run()
---
+++ indexer.py
@@ -0,0 +1,108 @@
+import lucene
+import pandas as pd
+import os
+
+from java.nio.file import Paths
+from org.apache.lucene.analysis.core import KeywordAnalyzer
+from org.apache.lucene.document import Document, Field, FieldType
+from org.apache.lucene.index import IndexOptions, IndexWriter, IndexWriterConfig, DirectoryReader
+from org.apache.lucene.store import MMapDirectory
+from org.apache.lucene.search import IndexSearcher
+from org.apache.lucene.queryparser.classic import QueryParser
+
+
+class Indexer:
+    def __init__(self):
+        self.enviroment = lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+        self.directory = MMapDirectory(Paths.get('index'))
+        self.parsed_data_path = "/workspaces/vinf_project/parsed/test_data.csv"
+        self.analyzer = KeywordAnalyzer()
+        self.index_folder = '/workspaces/vinf_project/index'
+
+    @staticmethod
+    def create_field_type(self):
+        field_type = FieldType()
+        field_type.setStored(True)
+        field_type.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
+        return field_type
+
+    @staticmethod
+    def delete_files_in_subfolder(self):
+        try:
+            # list all files in the subfolder
+            files = os.listdir(self.index_folder)
+
+            # iterate over the files and delete each one
+            for file_name in files:
+                file_path = os.path.join(self.index_folder, file_name)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+                    print(f"Deleted file: {file_path}")
+
+            print("All files in the subfolder have been deleted.")
+
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+    # create index for parsed data
+    def indexer(self):
+        self.delete_files_in_subfolder(self)
+        try:
+            # load parsed data
+            csv_data = pd.read_csv(self.parsed_data_path)
+            print(f"Loaded {csv_data.shape[0]} rows from {self.parsed_data_path}")
+
+            # lucene indexer
+            writer = IndexWriter(self.directory, IndexWriterConfig(self.analyzer))
+
+            # set field type
+            field_type = self.create_field_type(self)
+
+            # iterate over CSV 
+            for idx, row in csv_data.iterrows():
+                # lucene document
+                doc = Document()
+
+                # add fields
+                # row 0 - title (name of the race), row 1 - year, row 9 - departure (place), row 10 - arrival (place)
+                for i in [0, 1, 9, 10]:
+                    column_name = csv_data.columns[i]
+                    doc.add(Field(column_name, str(row[column_name]), field_type))
+
+                # add the document to the index
+                writer.addDocument(doc)
+
+            print(f"indexed")
+            writer.commit()
+            writer.close()
+
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+    def search(self):
+        reader = DirectoryReader.open(self.directory)
+        searcher = IndexSearcher(reader)
+
+        parser = QueryParser("title", KeywordAnalyzer())
+        # get race name from user 
+        print("potential races: Tour de France, Giro d'Italia, Vuelta a España, Okolo Slovenska,... ")
+        race_name = input("Enter the name of the race: ")
+        query = parser.parse(race_name)
+
+        records = searcher.search(query, 1500).scoreDocs
+
+        # iterate through results:
+        for record in records:
+            recordDoc = searcher.doc(record.doc)
+            if recordDoc['departure'] != 'nan':
+                print(recordDoc['title'], recordDoc['year'], "departure: ", recordDoc['departure'])
+
+        print('Number of results found for the race',len(records))
+
+
+if __name__ == '__main__':
+    index = Indexer()
+    is_index = input("Do you want to use index?: (y/n)")
+    if is_index == 'y':
+        index.indexer()
+    index.search()
